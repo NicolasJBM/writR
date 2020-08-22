ee31c8d831903b93068517d8c3120221bdc38b84
NicolasJBM <31648860+NicolasJBM@users.noreply.github.com>
2020-08-22 10:26:42 +0200

    Remove charts

--- inst/scripts/scripts.R
+++ inst/scripts/scripts.R
@@ -10,8 +10,3 @@ save(journals, file="data/journals.RData")
 
 jelcodes <- read.csv("inst/scripts/jelcodes.csv", stringsAsFactors = FALSE)
 save(jelcodes, file="data/jelcodes.RData")
-
-
-
-charts <- read.csv("inst/scripts/charts.csv", stringsAsFactors = FALSE)
-save(charts, file="data/charts.RData")
